91c7841bf207e5ebb57a24b2bf4caf216c125ecd
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-09-29 15:57:56 -0300

    Update mips1.asm

--- Códigos - Assembly MIPS/mips1.asm
+++ Códigos - Assembly MIPS/mips1.asm
@@ -1,10 +1,11 @@
 .data 
 	# .data é uma área para dados na memória principal 
+	
 	# msg -> variável; .sciiz -> tipo da variável; entre aspas a mensagem a ser exibida 
 	msg: .asciiz "Olá, mundo!"
 .text
 	#.text é uma área para instruções do programa 
-	
+
 	li $v0, 4 #instrução para impressão de String
 	la $a0, msg # load adress (la) vai indicar o endereço em que está a mensagem 
 	syscall  #comando para imprimir
