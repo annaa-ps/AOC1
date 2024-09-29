ce4b47e4ccb15b13c34b26c852c0b861875f0e26
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-09-29 16:00:21 -0300

    Update código2.asm

--- Códigos - Assembly MIPS/código2.asm
+++ Códigos - Assembly MIPS/código2.asm
@@ -1,9 +1,9 @@
 #Programa para imprimir apenas um caractere 
 .data
 	#caractere -> nome da variável; .byte -> tipo da variável; 'A' -> caractere a ser impresso 
-	caractere: .byte 'A'  
+	caractere:.byte 'A'  
 .text 
 	li $v0, 4 #instrução para imprimir char ou string 
-	la $a0, caractere # la (load adress) para colocar no registrador a0 (que no caso é caractere) 
-	                  # o que precisa ser impresso pelo syscall 
+	la $a0, caractere #la (load adress) para colocar no registrador a0 (que no caso é caractere) 
+	                  #o que precisa ser impresso pelo syscall 
 	syscall  
